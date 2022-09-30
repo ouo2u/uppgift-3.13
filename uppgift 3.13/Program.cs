d015860bd0210246acd78835cf96386627d3b5a5
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-30 10:19:09 +0200

    Add project files.

---
+++ uppgift 3.13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift_3._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hur gammal är du?Svara I hela antal år");
+            int ålder = int.Parse(Console.ReadLine());
+
+
+            if (ålder > 19)
+            {
+                Console.WriteLine("du är för gammal för att vara med I tavlingen");
+            }
+            else if(ålder > 16)
+            {
+                Console.WriteLine("du kan vara med I tavlingen");
+            }
+            else 
+            {
+                Console.WriteLine("du är för ung för att vara med I tavlingen");
+            }
+
+        }
+    }
+}
